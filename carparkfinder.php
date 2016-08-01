db5b47c376f66f6e5a3618c171284cbd6ed47fbf
Gary Mok <garyjmok@aol.com>
2016-08-01 22:13:31 +0000

    Added car park database and wrote php code to obtain data from database

--- carparkfinder.php
+++ carparkfinder.php
@@ -1,5 +1,27 @@
+<p>
+
 <?php
 
-echo 'Car park says hello and goodbye.';
+$db = new mysqli('localhost', 'root', 'root', 'CARPARKFINDER');
+
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+
+$sql = <<<SQL
+SELECT * FROM car_parks WHERE id = 1
+SQL;
+
+if(!$result = $db->query($sql)){
+    die('There was an error running the query [' . $db->error . ']');
+}
+
+while($row = $result->fetch_assoc()){
+    echo 'Name: ' . $row['Name'] . '<br />';
+    echo 'Owner: ' . $row['Owner'] . '<br />';
+    echo 'Vacancies: ' . $row['Vacancies'] . '<br />';
+}
 
 ?>
+
+</p>
