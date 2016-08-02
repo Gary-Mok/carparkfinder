e0c9b44ba6a0c63992034f7c3f6556ac7aefb386
Gary Mok <garyjmok@aol.com>
2016-08-02 10:54:01 +0000

    Input fields in form php, app php echoes data from form inputs

--- carparkfinder.php
+++ carparkfinder.php
@@ -20,6 +20,9 @@ if(!$result = $db->query($sql)){
     die('There was an error running the query [' . $db->error . ']');
 }
 
+echo 'Location: ' . $_POST["location"] . '<br />';
+echo 'Postcode: ' . $_POST["postcode"] . '<br /><br />';
+
 while($row = $result->fetch_assoc()){
     echo 'Name: ' . $row['Name'] . '<br />';
     echo 'Vacancies: ' . $row['Vacancies'] . '<br />';
--- carparkfinderform.php
+++ carparkfinderform.php
@@ -1,5 +1,27 @@
-<?php
+<html>
 
-echo 'Input location or postcode.';
+<body>
 
-?>
+<div>
+
+    <?php
+
+    echo 'Input location or postcode.';
+
+    ?>
+
+</div>
+
+<div>
+
+    <form action="carparkfinder.php" method="post">
+        Location: <input type="text" name="location"><br>
+        Postcode: <input type="text" name="postcode"><br>
+        <input type="submit">
+    </form>
+
+</div>
+
+</body>
+
+</html>
