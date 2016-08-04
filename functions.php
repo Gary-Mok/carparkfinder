d7b2f987237beb2458d399c7bda76c88a17dbe7e
Gary Mok <garyjmok@aol.com>
2016-08-04 12:21:51 +0000

    ticket-14 demonstrate how to use return statement and simpify overall code minor edits to variable names moved inline css declarations to separate css files Addressed a majority of issues with previous version of code Displayed results for car parks and viewing whole database is shown in a table; other minor edits creating records does not require all details, updating records is done in one go, added comments, other minor changes Cut down nested if statements, moved database connection to functions.php

---
+++ functions.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Sanitise (sort of) user input
+ *
+ * @param string $data
+ *
+ * @return string
+ */
+function input($data)
+{
+    return htmlspecialchars(stripslashes(trim($data)));
+}
+
+/**
+ * @param array $elements
+ *
+ * @return string
+ */
+function generateElements(array $elements)
+{
+    if (count($elements) === 0) {
+        return '';
+    }
+
+    $html = '';
+    foreach ($elements as $elementName => $properties) {
+        $html .= '
+        <div class="element ' . $elementName . '">
+        ';
+
+        if (!isset($properties['label']) || false !== $properties['label']) {
+            $html .= '<label for="' . $elementName . '">' . $properties['description'] . ':</label>';
+        }
+
+        $html .= '
+            <input type="' . $properties['type'] . '" name="' . $elementName . '" id="' . $elementName . '">
+        </div>
+        ' . PHP_EOL;
+    }
+
+    return $html;
+}
+
+/**
+ * @param array $elements
+ * @param array $requests
+ * @param mysqli $db
+ *
+ * @return string
+ */
+function getCarparkSearchQuery(array $elements, array $requests, $db)
+{
+    $baseQuery = 'SELECT * FROM car_parks ';
+    $searchTerms = array();
+
+    foreach ($elements as $elementName => $properties) {
+        if (!isset($requests[$elementName])
+            || (isset($properties['isSearchable']) && false === $properties['isSearchable'])
+        ) {
+            continue;
+        }
+
+        $field = trim($requests[$elementName]);
+
+        if (strlen($field) === 0) {
+            continue;
+        }
+
+        $searchTerms[] = sprintf('%s LIKE "%%%s%%"', $elementName, mysqli_real_escape_string($db, $field));
+    }
+
+    if (count($searchTerms) === 0) {
+        return $baseQuery;
+    }
+
+    return $baseQuery . ' WHERE ' . implode(' OR ', $searchTerms);
+}
---
+++ search.php
@@ -0,0 +1,68 @@
+<html>
+
+<head>
+    <link type='text/css' rel='stylesheet' href='/style/main.css'/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>Car Park Finder</title>
+</head>
+
+<body class="carpark-finder">
+
+<?php
+
+include 'bootstrap.php';
+
+$elements = array(
+    'name' => array(
+        'description' => 'Carpark Name',
+        'isRequired' => false,
+        'type' => 'text',
+    ),
+    'location' => array(
+        'description' => 'Location',
+        'isRequired' => false,
+        'type' => 'text',
+    ),
+    'postcode' => array(
+        'description' => 'Postcode',
+        'isRequired' => false,
+        'type' => 'text',
+    ),
+    'submit' => array(
+        'description' => 'Submit',
+        'isRequired' => false,
+        'type' => 'submit',
+        'label' => false,
+        'isSearchable' => false,
+    ),
+);
+?>
+
+<div class="filter">
+    <h1>Car Park Finder</h1>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+        <?php echo generateElements($elements) ?>
+    </form>
+</div>
+
+<?php if ('POST' === $_SERVER['REQUEST_METHOD']) : ?>
+    <div class="result">
+        <h1>Result</h1>
+        <?php
+        if (!$result = $db->query($query = getCarparkSearchQuery($elements, $_REQUEST, $db))) {
+            die('There was an error running the query [' . $db->error . ']'); //error message if query fails
+        }
+
+        echo '<table><tr><th>Name</th><th>Location</th><th>Postcode</th><th>Vacancies</th></tr>';
+
+        while ($row = $result->fetch_array()) {
+            echo '<tr><td>' . $row['name'] . '</td><td>' . $row['location'] . '</td><td>' . $row['postcode'] . '</td><td>' . $row['vacancies'] . '</td></tr>';
+        }
+
+        echo '</table>';
+        ?>
+    </div>
+<?php endif; ?>
+
+</body>
+</html>
