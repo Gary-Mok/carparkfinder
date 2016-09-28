ac7f8df638a320093614cfb00fcb2330a84f869e
Gary Mok <garyjmok@aol.com>
2016-09-28 00:32:39 +0000

    new transaction table, still need to factor in all other details

--- credit.php
+++ credit.php
@@ -44,27 +44,7 @@ if ($_SESSION['type'] == "visitor") {
 
 <div>
 
-    <p>Your current credit is:
-
-    <?php
-
-    $id = $_SESSION['id'];
-
-    $sql = 'SELECT transactions.credit FROM transactions WHERE member_id = :id AND id = (SELECT MAX(id) FROM transactions WHERE member_id = :id)';
-    $query = $db->prepare($sql);
-    $credit = $query->execute(['id' => $id]);
-
-    if ($credit === false) {
-        die('There was an error running the query [' . $db->errorInfo() . ']'); //error message if query fails
-    }
-
-    $row = $query->fetch(PDO::FETCH_ASSOC);
-
-    echo $row['credit'];
-
-    ?>
-
-    </p>
+    <p>Your current credit is:</p>
 
     <p>Add credit</p>
 
@@ -76,20 +56,45 @@ if ($_SESSION['type'] == "visitor") {
 
     $id = $_SESSION['id'];
 
-    $sql = 'SELECT transaction_type.description, transactions.credit, transactions.create_at FROM transactions INNER JOIN transaction_type ON transactions.transaction_type_id=transaction_type.id WHERE member_id LIKE :id ORDER BY transactions.create_at ASC';
+    $sql = 'SELECT transactions.create_at, transaction_type.description, transactions.credit FROM transactions INNER JOIN transaction_type ON transactions.transaction_type_id=transaction_type.id WHERE member_id = :id';
     $query = $db->prepare($sql);
-    $trans = $query->execute(['id' => $id]);
+    $credit = $query->execute(['id' => $id]);
 
-    if ($trans === false) {
+    if ($credit === false) {
         die('There was an error running the query [' . $db->errorInfo() . ']'); //error message if query fails
     }
 
-    echo '<table><tr><th>Transaction</th><th>Credit</th><th>Date</th></tr>';
+    $dateEntries = array();
 
-    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-        echo '<tr class="tableContents"><td>' . $row['description'] . '</td><td>' . $row['credit'] . '</td><td>' . $row['create_at'] . '</td></tr>';
-        //database displayed in a table
+    $descriptionEntries = array();
+
+    $creditEntries = array();
+
+    while ($Columns = $query->fetch(PDO::FETCH_ASSOC)) {
+        array_push($dateEntries, $Columns['create_at']);
+        array_push($descriptionEntries, $Columns['description']);
+        array_push($creditEntries, $Columns['credit']);
     }
+
+    $runningTotal = 0;
+
+    echo '<table><tr><th>Debit</th><th>Credit</th><th>Running Total</th></tr>';
+
+    foreach($creditEntries as $entry) {
+        $row = array();
+        if($entry < 0) {
+            $row['debit'] = abs($entry);
+            $row['credit'] = 0;
+        }
+        if($entry > 0) {
+            $row['debit'] = 0;
+            $row['credit'] = abs($entry);
+        }
+        $runningTotal = $runningTotal - $row['debit'] + $row['credit'];
+        $row['runningTotal'] = $runningTotal;
+        echo '<tr><td>' . implode('</td><td>', $row) . '</td></tr>';
+    }
+
     echo '</table>';
 
     ?>
