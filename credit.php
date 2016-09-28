8d21fce15dde662f9d072fe2fc5a4765006be1d1
Gary Mok <garyjmok@aol.com>
2016-09-28 00:32:39 +0000

    new transaction table, still need to factor in all other details

--- credit.php
+++ credit.php
@@ -1,8 +1,7 @@
 <html>
 
 <head>
-    <link type='text/css' rel='stylesheet' href='/style/backend.css' />
-
+    <link type='text/css' rel='stylesheet' href='/style/backend.css'/>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
     <title>
@@ -21,17 +20,16 @@ if (!isset($_SESSION['username'])) {
     exit();
 }
 
-if ($_SESSION['type'] == "visitor") {
+if (isset($_SESSION['type']) && $_SESSION['type'] === "visitor") {
     echo 'You do not have the administrative right to view this page. Please return to the <a href="search.php">main page</a>.';
+
     return '';
 }
 
 ?>
 
 <div>
-
     <h1>Car Park Finder</h1>
-
 </div>
 
 <?php include 'navigation.php' ?>
@@ -44,27 +42,7 @@ if ($_SESSION['type'] == "visitor") {
 
 <div>
 
-    <p>Your current credit is:
-
-    <?php
-
-    $id = $_SESSION['id'];
-
-    $sql = 'SELECT transactions.credit FROM transactions WHERE member_id = :id AND id = (SELECT MAX(id) FROM transactions WHERE member_id = :id)';
-    $query = $db->prepare($sql);
-    $credit = $query->execute(['id' => $id]);
-
-    if ($credit === false) {
-        die('There was an error running the query [' . $db->errorInfo() . ']'); //error message if query fails
-    }
-
-    $row = $query->fetch(PDO::FETCH_ASSOC);
-
-    echo $row['credit'];
-
-    ?>
-
-    </p>
+    <p>Your current credit is:</p>
 
     <p>Add credit</p>
 
@@ -76,26 +54,40 @@ if ($_SESSION['type'] == "visitor") {
 
     $id = $_SESSION['id'];
 
-    $sql = 'SELECT transaction_type.description, transactions.credit, transactions.create_at FROM transactions INNER JOIN transaction_type ON transactions.transaction_type_id=transaction_type.id WHERE member_id LIKE :id ORDER BY transactions.create_at ASC';
-    $query = $db->prepare($sql);
-    $trans = $query->execute(['id' => $id]);
+    $sql = 'SELECT transactions.create_at, transaction_type.description, transactions.credit 
+FROM transactions 
+INNER JOIN transaction_type ON transactions.transaction_type_id=transaction_type.id 
+WHERE member_id = :id';
 
-    if ($trans === false) {
+    $query = $db->prepare($sql);
+    $credit = $query->execute(['id' => $id]);
+
+    if (false === $credit) {
         die('There was an error running the query [' . $db->errorInfo() . ']'); //error message if query fails
     }
 
-    echo '<table><tr><th>Transaction</th><th>Credit</th><th>Date</th></tr>';
-
+    $runningTotal = 0;
+    $entries = array();
     while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-        echo '<tr class="tableContents"><td>' . $row['description'] . '</td><td>' . $row['credit'] . '</td><td>' . $row['create_at'] . '</td></tr>';
-        //database displayed in a table
+        $runningTotal += $row['credit'];
+        $entry = array();
+        $entry['transactionDescription'] = $row['description'];
+        $entry['debit'] = $row['credit'] < 0 ? abs($row['credit']) : 0;
+        $entry['credit'] = $row['credit'] > 0 ? abs($row['credit']) : 0;
+        $entry['runningTotal'] = $runningTotal;
+
+        $entries[] = $entry;
     }
+
+    echo '<table><tr><th>Transaction Description</th><th>Debit</th><th>Credit</th><th>Running Total</th></tr>';
+
+    foreach ($entries as $entry) {
+        echo '<tr><td>' . implode('</td><td>', $entry) . '</td></tr>';
+    }
+
     echo '</table>';
 
     ?>
-
 </div>
-
 </body>
-
 </html>
